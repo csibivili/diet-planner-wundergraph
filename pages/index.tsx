a6a00e1af48a3b2bec810314c9b0bf6a34c14dc7
Vili <csibi.vili@gmail.com>
2022-11-21 21:32:50 +0100

    feat: remove unnecessary element

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,7 @@ const Home: NextPage = () => {
     operationName: 'Recipes',
     input: {
       query,
-    },
-    liveQuery: true,
+    }
   })
   return (
     <div>
@@ -29,12 +28,6 @@ const Home: NextPage = () => {
               aria-label="Food query"
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button
-              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-              type="button"
-            >
-              Search
-            </button>
           </div>
         </form>
       </div>
